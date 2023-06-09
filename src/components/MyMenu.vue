18b9f9eec55df9b1676674a8245c49d99b6c13de
Zwanan-github <270603801@qq.com>
2023-06-09 22:19:01 +0800

    initproject

---
+++ src/components/MyMenu.vue
@@ -0,0 +1,82 @@
+<template>
+    <el-menu default-active="2" class="el-menu-vertical-demo" v-model="isCollapse" :collapse="isCollapse" @mouseover="fmouseover()" @mouseleave="fmouseleave()">
+        <el-sub-menu index="1">
+            <template #title>
+                <el-icon>
+                    <location />
+                </el-icon>
+                <span>Navigator One</span>
+            </template>
+            <el-menu-item-group>
+                <template #title><span>Group One</span></template>
+                <el-menu-item index="1-1">item one</el-menu-item>
+                <el-menu-item index="1-2">item two</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="Group Two">
+                <el-menu-item index="1-3">item three</el-menu-item>
+            </el-menu-item-group>
+            <el-sub-menu index="1-4">
+                <template #title><span>item four</span></template>
+                <el-menu-item index="1-4-1">item one</el-menu-item>
+            </el-sub-menu>
+            <el-sub-menu index="1-5">
+                <template #title><span>item four</span></template>
+                <el-menu-item index="1-5-1">item one</el-menu-item>
+            </el-sub-menu>
+            <el-sub-menu index="1-6">
+                <template #title><span>item four</span></template>
+                <el-menu-item index="1-6-1">item one</el-menu-item>
+            </el-sub-menu>
+            <el-sub-menu index="1-7">
+                <template #title><span>item four</span></template>
+                <el-menu-item index="1-7-1">item one</el-menu-item>
+            </el-sub-menu>
+            <el-sub-menu index="1-8">
+                <template #title><span>item four</span></template>
+                <el-menu-item index="1-8-1">item one</el-menu-item>
+            </el-sub-menu>
+        </el-sub-menu>
+        <el-menu-item index="2">
+            <el-icon><icon-menu /></el-icon>
+            <template #title>Navigator Two</template>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+            <el-icon>
+                <document />
+            </el-icon>
+            <template #title>Navigator Three</template>
+        </el-menu-item>
+        <el-menu-item index="4">
+            <el-icon>
+                <setting />
+            </el-icon>
+            <template #title>Navigator Four</template>
+        </el-menu-item>
+    </el-menu>
+</template>
+  
+<script setup>
+import { ref } from 'vue'
+import {
+    Document,
+    Menu as IconMenu,
+    Location,
+    Setting,
+} from '@element-plus/icons-vue'
+
+var isCollapse = ref(true)
+const fmouseover = () => {
+    isCollapse.value = false;
+}
+const fmouseleave = () => {
+    isCollapse.value = true;
+}
+
+</script>
+  
+<style>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,42 @@
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pluginOptions: {
+    electronBuilder: {
+        builderOptions: {
+            // 在这里的配置将会和默认配置合并，然后传递给electron-builder
+            appId: 'com.zwanan.app', // 项目唯一标识
+            productName: 'ikun-home', // 打包产物的前缀
+            copyright: 'Copyright © 2023 Zwanan', // 可用使用${}引用package.json里面配置项，配置项不存在会报错
+            directories: {
+                output: 'dist' // 打包产物的位置
+            },
+            // ------- windows 相关配置
+            win: {
+                icon: 'public/icons/icon.ico', // 应用图标
+                target: ['nsis'] // 打包的目标类型,支持很多类型，具体看文档
+            },
+            nsis: {
+                "oneClick": false,
+                "perMachine": true,
+                "allowToChangeInstallationDirectory": true // 允许修改安装目录
+            }
+        }
+    }
+  },
+  configureWebpack: {
+    plugins:[
+      AutoImport({
+        resolvers:[ElementPlusResolver()],
+      }),
+      Components({
+        resolvers:[ElementPlusResolver()],
+      })
+    ]
+  },
+  lintOnSave: false
 })
